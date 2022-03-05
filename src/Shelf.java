10071a2b02513c537ccd06f9a2efec1f827fdab0
YukiOkamoto0206 <okamotoyu@crunchtimer.jp>
2022-03-04 17:00:14 -0800

    add: constructor

--- src/Shelf.java
+++ src/Shelf.java
@@ -1,2 +1,18 @@
+import java.util.HashMap;
+
 public class Shelf {
+    public static final int SHELF_NUMBER_=0;
+    public static final int SUBJECT_=0;
+    private int shelfNumber;
+    private String subject;
+    private HashMap<Book, Integer> books;
+
+    public Shelf(int shelfNumber, String subject) {
+        this.shelfNumber = shelfNumber;
+        this.subject = subject;
+        this.books = new HashMap<>();
+    }
+
+
+
 }
