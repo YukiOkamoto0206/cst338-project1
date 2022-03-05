0b422ebe6c2e78ea710885257c6145c4699278a3
YukiOkamoto0206 <okamotoyu@crunchtimer.jp>
2022-03-04 17:05:45 -0800

    add: setter and getter, equals and hashcode, and toString method

--- src/Shelf.java
+++ src/Shelf.java
@@ -1,8 +1,9 @@
 import java.util.HashMap;
+import java.util.Objects;
 
 public class Shelf {
-    public static final int SHELF_NUMBER_=0;
-    public static final int SUBJECT_=0;
+    public static final int SHELF_NUMBER_ = 0;
+    public static final int SUBJECT_ = 0;
     private int shelfNumber;
     private String subject;
     private HashMap<Book, Integer> books;
@@ -13,6 +14,57 @@ public class Shelf {
         this.books = new HashMap<>();
     }
 
+    // addBook
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Shelf)) return false;
+        Shelf shelf = (Shelf) o;
+        return getShelfNumber() == shelf.getShelfNumber() && Objects.equals(getSubject(), shelf.getSubject());
+    }
 
+    // getBookCount
+
+    public HashMap<Book, Integer> getBooks() {
+        return books;
+    }
+
+    public int getShelfNumber() {
+        return shelfNumber;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getShelfNumber(), getSubject());
+    }
+
+    // listBooks
+
+    // removeBook
+
+    public void setBooks(HashMap<Book, Integer> books) {
+        this.books = books;
+    }
+
+    public void setShelfNumber(int shelfNumber) {
+        this.shelfNumber = shelfNumber;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    @Override
+    public String toString() {
+        return "Shelf{" +
+                "shelfNumber=" + shelfNumber +
+                ", subject='" + subject + '\'' +
+                ", books=" + books +
+                '}';
+    }
 }
