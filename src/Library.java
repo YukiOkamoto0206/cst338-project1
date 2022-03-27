e8d1d162845b0a3fbbdd129b3dc4ae94c8d15a25
YukiOkamoto0206 <okamotoyu@crunchtimer.jp>
2022-03-26 17:27:09 -0700

    add: variables to Library

--- src/Library.java
+++ src/Library.java
@@ -1,2 +1,11 @@
+import java.util.HashMap;
+import java.util.List;
+
 public class Library {
+    public static int LENDING_LIMIT = 5;
+    private String name;
+    private static int libraryCard;
+    private List<Reader> readers;
+    private HashMap<String, Shelf> shelves;
+    private HashMap<Book, Integer> books;
 }
